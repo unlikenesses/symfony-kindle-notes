14927df837565e87024d373f52b3c87e9013919e
Unlikenesses <unlikenesses@users.noreply.github.com>
2019-10-30 11:46:19 +0100

    Update BookFixtures.php

--- src/DataFixtures/BookFixtures.php
+++ src/DataFixtures/BookFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use App\Entity\Book;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
@@ -9,19 +10,31 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class BookFixtures extends Fixture implements FixtureGroupInterface
 {
+    private $user;
+    
     public static function getGroups(): array
     {
         return ['testing'];
     }
+    
+    private function createUser(): void
+    {
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $manager->persist($user);
+        $this->user = $user;
+    }
 
     public function load(ObjectManager $manager)
     {
+        $this->createUser();
         $book = new Book();
         $book->setTitle('Test title')
             ->setTitleString('Test title')
             ->setAuthorFirstName('Fred')
             ->setAuthorLastName('Smith')
-            ->setYear('1980');
+            ->setYear('1980')
+            ->setUser($this->user);
         $manager->persist($book);
 
         $manager->flush();
