5ea15bed81610cdb8bae8f80d7f01754921ea158
Alex Pestell <alex.pestell@gmail.com>
2020-02-19 21:31:14 +0100

    Add paperwhite title string parser test.

---
+++ tests/Service/Parser/Paperwhite/PaperwhiteTitleStringParserTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests\Service\Parser\Paperwhite;
+
+use App\ValueObject\Author;
+use App\Service\Parser\Paperwhite\PaperwhiteTitleStringParser;
+use PHPUnit\Framework\TestCase;
+
+class PaperwhiteTitleStringParserTest extends TestCase
+{
+    private $parser;
+
+    public function setUp()
+    {
+        $this->parser = new PaperwhiteTitleStringParser();
+    }
+
+    /**
+     * @dataProvider getTitleStringTests
+     */
+    public function testItCanParseATitleString(string $titleString, string $expectedTitle, Author $expectedAuthor)
+    {
+        $this->parser->parse($titleString);
+        $this->assertEquals($expectedTitle, $this->parser->getTitle());
+        $this->assertEquals($expectedAuthor, $this->parser->getAuthor());
+    }
+
+    public function getTitleStringTests()
+    {
+        return [
+            // titleString, title, author
+            [
+                'Century of Revolution, 1603-1714 - Christopher Hill',
+                'Century of Revolution, 1603-1714',
+                new Author('Christopher', 'Hill'),
+            ],
+            [
+                'Rights of War and Peace (2005 ed.) vol. 1 (Book I) (Hugo Grotius)',
+                'Rights of War and Peace (2005 ed.) vol. 1 (Book I)',
+                new Author('Hugo', 'Grotius'),
+            ],
+        ];
+    }
+}
